c60bac32bd33b8a9d36c5cfb01e86821255a5345
joey2262 <connect@josephdechant.com>
2020-02-27 14:29:26 -0700

    addded Sass

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
+import React  from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state={
+    hex : {
+      color : '000000',
+    },
+    rgba : {
+      r : 0,
+      g : 0,
+      b : 0,
+      a : 0
+    }
+  }
+  
+  updateHex = event => {
+    console.log(event.keyCode);
+    console.log(event.target.selectionStart)
+    let hex = this.state.hex;
+    hex.color = event.target.value;
+
+    this.setState({hex});
+  
+  }
+
+  // hexToRgb = (hex) => {
+  // }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="content-wrap">
+            <div className="inputs-wrap">
+              <div className="hex-wrap" >
+                <h2>Hex Value</h2>
+                <p className="hex-input">
+                <label>Alpha-Numeric Value</label>
+                <input value={this.state.hex.color} onKeyUp={(event)=>{this.updateHex(event)}} onChange={(event)=>{this.getCaret(event)}}/>
+                </p>
+              </div>
+              <div className="rgba-wrap">
+                <h2 className="rgba-head">RGB(A)</h2>
+                <p className="red-input">
+                <label>Red</label>
+                <input placeholder="red" value="0"/>
+                </p>
+                <p className="green-input">
+                <label>Green</label>
+                <input placeholder="green" value="0"/>
+                </p>
+                <p className="blue-input">
+                <label>Blue</label>
+                <input placeholder="blue" value="0"/>
+                </p>
+                <p className="aplha-input">
+                <label>Alpha</label>
+                <input placeholder="alpha" value="0"/>
+                </p>
+              </div>
+            </div>
+            <div className="results-column">
+              <div className="color-banner" style={{backgroundColor : '#'+this.state.hex}}></div>
+              <div className="numeric-results-wrap">
+                <div className="hex-results">
+                    <code>{'#' +this.state.hex}</code>
+                </div>
+                <div className="rgba-results">
+                  <code>{`rgba(${this.state.rgba.r},${this.state.rgba.g},${this.state.rgba.b},${this.state.rgba.a})`}</code>
+                </div>
+              </div>
+            </div>
+            </div>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
